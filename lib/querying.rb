8fdf8c2d82d61c650aa485c74ae40f1dc62f43cf
Rob Cobb <rwcobbjr@gmail.com>
2018-03-19 17:21:44 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, year FROM books JOIN series ON books.series_id = series.id WHERE series.id = (SELECT MIN(id) FROM series)"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.name) FROM characters GROUP BY characters.species ORDER BY COUNT(characters.name) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors JOIN series on series.author_id = authors.id JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series JOIN characters ON characters.series_id = series.id GROUP BY series.id ORDER BY (SELECT COUNT(*) from characters WHERE characters.species = 'human') LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.id) FROM characters JOIN character_books on character_books.character_id = characters.id JOIN books ON books.id = character_books.book_id GROUP BY characters.id ORDER BY COUNT(books.id) DESC, characters.name ASC;"
 end
